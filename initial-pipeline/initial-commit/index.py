8efbd83cb2c8b48303e20e824dcbe1b875e72fbd
HardikThakkar94 <srinjoy@amazon.com>
2021-12-08 01:27:40 -0500

    Patched Vulnerabilities :hammer:

--- initial-pipeline/initial-commit/index.py
+++ initial-pipeline/initial-commit/index.py
@@ -9,4 +9,4 @@ access_key = "AKIAZELJK6BJF5234HAC"
 def hello():
     return "Hello World!"
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("5000"), debug=True)
+    app.run(host="0.0.0.0", port=int("5000"), debug=False)
